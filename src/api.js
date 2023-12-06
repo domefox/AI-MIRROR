a5aec798700c0b6619bbb957b11654acc56a1399
alextli <49765987+alextli@users.noreply.github.com>
2023-12-05 19:38:18 -0500

    disabled safety checks

--- src/api.js
+++ src/api.js
@@ -14,6 +14,7 @@ export async function generateImage(frame) {
       input: {
         prompt: 'abstract cubist painting in the style of picasso',
         image_url: frame,
+        enable_safety_checks: false
       },
       logs: true,
       onQueueUpdate: (update) => {
