08fe4e99b0734034ee8e4c36a139e99a5c137667
alextli <49765987+alextli@users.noreply.github.com>
2023-12-06 21:23:36 -0500

    consolidated files and got framerate unlocked from render cycle

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import React, { useState } from 'react';
 import WebcamCapture from './WebcamCapture';
-import Api from './api';
 
 function App() {
-  const [capturedImage, setCapturedImage] = useState(null);
 
   return (
     <div className="App">
-      <WebcamCapture setCapturedImage={setCapturedImage} />
-      <Api frame={capturedImage} />
+      <WebcamCapture />
     </div>
   );
 }
