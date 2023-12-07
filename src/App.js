1e883fcb39c59f05bfb994fb86b04bd25630bfff
alextli <49765987+alextli@users.noreply.github.com>
2023-12-06 19:09:01 -0500

    simplified file structure

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React, { useState } from 'react';
-import DisplayAI from './DisplayAI';
 import WebcamCapture from './WebcamCapture';
 import Api from './api';
 
 function App() {
   const [capturedImage, setCapturedImage] = useState(null);
-  const [mirrorImage, setMirrorImage] = useState(null);
 
   return (
     <div className="App">
-      <DisplayAI mirrorImage={mirrorImage} />
       <WebcamCapture setCapturedImage={setCapturedImage} />
-      <Api frame={capturedImage} setMirrorImage={setMirrorImage} />
+      <Api frame={capturedImage} />
     </div>
   );
 }
--- src/api.js
+++ src/api.js
@@ -1,40 +1,46 @@
 // checking I have updated properly
 
 import * as fal from "@fal-ai/serverless-client";
+import {useState} from 'react'
 
 
 export function Api({frame, setMirrorImage}) {
   //console.log('generateImage is called'); // Add this line
-  try {
-    fal.config({
-      // Can also be auto-configured using environment variables:
-      // Either a single FAL_KEY or a combination of FAL_KEY_ID and FAL_KEY_SECRET
-      credentials: `84bf17d7-ed75-4782-a608-a7a165cf59e2:af97d58be14f1cb3fb1ec0b5917edd83`,
-    });
-    const connection = fal.realtime.connect("110602490-lcm-sd15-i2i", {
-      connectionKey: '84bf17d7-ed75-4782-a608-a7a165cf59e2:af97d58be14f1cb3fb1ec0b5917edd83',
-      onError: (error) => {
-        console.error(error)
-      },
-      onResult: (result) => {
-        if(result.images && result.images[0]){
-          setMirrorImage(result.images[0].url);
-        }
+  const [image, setImage] = useState(null);
+  fal.config({
+    // Can also be auto-configured using environment variables:
+    // Either a single FAL_KEY or a combination of FAL_KEY_ID and FAL_KEY_SECRET
+    credentials: `84bf17d7-ed75-4782-a608-a7a165cf59e2:af97d58be14f1cb3fb1ec0b5917edd83`,
+  });
+  const connection = fal.realtime.connect("110602490-lcm-sd15-i2i", {
+    connectionKey: '84bf17d7-ed75-4782-a608-a7a165cf59e2:af97d58be14f1cb3fb1ec0b5917edd83',
+    onError: (error) => {
+      console.error(error)
+    },
+    onResult: (result) => {
+      if(result.images && result.images[0]){
+        setImage(result.images[0].url);
       }
-    });
+    }
+  });
 
-    connection.send({
-      image_url: frame, 
-      prompt: "Make this picture look like a child drew it. the subject is a person in front of a webcam. Try to stick closely to the colors in the picture.",
-      strength: 0.8,
-      guidance_scale: 2,
-      negative_prompt: "blurry, low resolution",
-      enable_safety_checks: false
-    });
+  connection.send({
+    image_url: frame, 
+    prompt: "Make this picture look like a child drew it. the subject is a person in front of a webcam. Try to stick closely to the colors in the picture.",
+    strength: 0.2,
+    guidance_scale: 2,
+    negative_prompt: "blurry, low resolution",
+    enable_safety_checks: false
+  });
 
-  } catch (error) {
-    console.error('Error in generateImage:', error);
-  }
+  return (
+    <img 
+      src={image} 
+      alt="AI Generated" 
+      style={{position: "absolute", top: "0", left: "0", width: "100vw", height: "100vh", objectFit: "contain"}} 
+    />
+  );
+
 }
 
 export default Api;
