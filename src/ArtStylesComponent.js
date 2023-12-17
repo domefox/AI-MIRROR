affdc8b952de0177d40df651f87afa003f901a82
unknown <weberwong@gmail.com>
2023-12-17 00:41:57 -0500

    prompt style added

--- src/ArtStylesComponent.js
+++ src/ArtStylesComponent.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import { ReactP5Wrapper } from 'react-p5-wrapper';
-import ArtStylesSlider from './ArtStylesSlider'; // Import your sketch function
+import React, { useState } from 'react';
 
 function ArtStylesComponent({ setPrompt }) {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleInputSubmit = (event) => {
+    event.preventDefault();
+    setPrompt.current = inputValue;
+    setInputValue('');
+  };
+
   return (
-    <div style={{ position: 'absolute', zIndex: 2}}>
-      <ReactP5Wrapper sketch={(p5) => ArtStylesSlider(p5, setPrompt)} />
+    <div style={{ position: 'absolute', bottom: 0, width: '100%', zIndex: 2 }}>
+      <form onSubmit={handleInputSubmit}>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Enter your prompt here"
+          style={{ width: '100%', padding: '10px' }}
+        />
+      </form>
     </div>
   );
 }
